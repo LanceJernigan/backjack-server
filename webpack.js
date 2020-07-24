f44a26a2d6f31fce64ca362f598b232bd8dc6907
Lance Jernigan <lance@lancejernigan.com>
2020-07-24 15:39:47 -0400

    Update produciton webpack

---
+++ webpack.js
@@ -0,0 +1,58 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+
+const config = {
+  entry: './src/index.js',
+  mode: 'production',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'scripts/[name].js',
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1];
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+      },
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: /(src)/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: 'public',
+      },
+    ]),
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+    new CompressionPlugin(),
+  ],
+};
+
+module.exports = config;
