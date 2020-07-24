5e8d6683a5fb308f773568f96e59e7c6b8aa42ab
Lance Jernigan <lance@lancejernigan.com>
2020-07-24 17:48:56 -0400

    log load

--- src/context/app.js
+++ src/context/app.js
@@ -2,13 +2,12 @@ import React, { createContext, useReducer } from 'react';
 import castReceiver from 'castReceiver';
 
 const context = castReceiver.framework.CastReceiverContext.getInstance();
+const playerManager = context.getPlayerManager();
 context.start();
 
-const initialState = { players: context.getSenders(), reciverContext: context };
+const initialState = { players: [], reciverContext: context };
 const appContext = createContext(initialState);
 const { Provider } = appContext;
-
-console.log(initialState, 'initial state');
 
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer((state, action) => {
@@ -19,6 +18,13 @@ const AppProvider = ({ children }) => {
         throw new Error();
     }
   }, initialState);
+
+  console.log(context.getSenders, 'pre load');
+
+  playerManager.setMessageInterceptor(
+    castReceiver.framework.messages.MessageType.LOAD,
+    () => console.log(context.getSenders(), 'loaded')
+  );
 
   context.addEventListener('SENDER_CONNECTED', (...props) =>
     dispatch({
